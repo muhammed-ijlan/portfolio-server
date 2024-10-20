b4e02e5f7a10efa66992badf4b781c0078c60a2f
Muhammed Ijlan <ijlanijlu580@gmail.com>
2024-10-20 11:02:39 +0530

    enquiry updates

--- models/enquiryModel.js
+++ models/enquiryModel.js
@@ -1,28 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const serviceSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    enum: [
-      "ISO_Management_System",
-      "HSE",
-      "Human_Resources",
-      "Financial",
-      "Business_Excellence",
-      "Sustainability",
-      "Product_Certification",
-      "Inspection_Services",
-    ],
-  },
-  serviceType: {
-    type: [String],
-    required: true,
-    enum: ["CONSULTANCY", "TRAINING", "API", "CONSULTANCY_AND_ADVISORY", "INSPECTION"],
-  },
-});
+
 
 const enquirySchema = new Schema(
   {
@@ -37,12 +16,7 @@ const enquirySchema = new Schema(
       required: true,
       trim: true,
     },
-    phone: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    organisation: {
+    subject: {
       type: String,
       required: true,
       trim: true,
@@ -51,30 +25,6 @@ const enquirySchema = new Schema(
       type: String,
       trim: true,
     },
-    // source: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "enquirySource",
-    //   required: true,
-    //   index: true
-    // },
-    // teleCaller: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "employees",
-    //   required: true,
-    //   index: true
-    // },
-    // enquiryManager: {
-    //   type: Schema.Types.ObjectId,
-    //   ref: "employees",
-    //   required: true,
-    //   index: true
-    // },
-    servicesFor: {
-      type: String,
-      required: true,
-      enum: ["SELF", "COMPANY"],
-    },
-    services: [serviceSchema],
   },
   { timestamps: true }
 );
--- routes/enquiryRouter.js
+++ routes/enquiryRouter.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const { body, query } = require("express-validator");
 const { enquiryController } = require("../controllers/_index");
 const {
-  verifyAdmin: { verifyAdmin },
   verifySuperAdmin: { verifySuperAdmin },
 } = require("../middlewares/_index");
 
@@ -17,41 +16,37 @@ const adminGetEnquiriesValidator = [
     .custom((val) => val > 0),
   query("name").optional({ checkFalsy: true }),
   query("email").optional({ checkFalsy: true }),
-  query("phone").optional({ checkFalsy: true }),
-  query("organisation").optional({ checkFalsy: true }),
-  query("serviceFor").optional({ checkFalsy: true }).isIn(["SELF", "COMPANY"]),
-  query("startDate").optional({ checkFalsy: true }),
-  query("endDate").optional({ checkFalsy: true }),
-  query("service").optional({ checkFalsy: true }),
+  query("subject").optional({ checkFalsy: true }),
+];
+const adminGetOneEnquiryValidator = [
+  query("id").isMongoId().notEmpty(),
 ];
 
-const exportEnquiriesValidator = [
-  body("name").optional({ checkFalsy: true }),
-  body("email").optional({ checkFalsy: true }),
-  body("phone").optional({ checkFalsy: true }),
-  body("organisation").optional({ checkFalsy: true }),
-  body("serviceFor").optional({ checkFalsy: true }).isIn(["SELF", "COMPANY"]),
-  body("startDate").optional({ checkFalsy: true }),
-  body("endDate").optional({ checkFalsy: true }),
-  body("service").optional({ checkFalsy: true }),
-];
-///////// get all enquiries //////
-router.get("/all", adminGetEnquiriesValidator, verifyAdmin, enquiryController.getAllEnquiries);
+// const exportEnquiriesValidator = [
+//   body("name").optional({ checkFalsy: true }),
+//   body("email").optional({ checkFalsy: true }),
+//   body("phone").optional({ checkFalsy: true }),
+//   body("organisation").optional({ checkFalsy: true }),
+//   body("serviceFor").optional({ checkFalsy: true }).isIn(["SELF", "COMPANY"]),
+//   body("startDate").optional({ checkFalsy: true }),
+//   body("endDate").optional({ checkFalsy: true }),
+//   body("service").optional({ checkFalsy: true }),
+// ];
+// ///////// get all enquiries //////
+router.get("/all", adminGetEnquiriesValidator, verifySuperAdmin, enquiryController.getAllEnquiries);
+router.get("/one", adminGetOneEnquiryValidator, verifySuperAdmin, enquiryController.getOneEnquiry);
 
 ///////// export enquiries //////
-router.post("/export", exportEnquiriesValidator, enquiryController.exportEnquiries);
+// router.post("/export", exportEnquiriesValidator, enquiryController.exportEnquiries);
 
 /////////////////////////////////////////// user Routes //////////////////////////////////////////////////
 
 const enquiryAddValidator = [
   body("name").trim().notEmpty(),
   body("email").trim().notEmpty(),
-  body("phone").trim().notEmpty(),
-  body("organisation").trim().notEmpty(),
+  body("subject").trim().notEmpty(),
   body("message").trim().optional({ checkFalsy: true }),
-  body("servicesFor").trim().notEmpty().isIn(["SELF", "COMPANY"]),
-  body("services").isArray().notEmpty(),
-];
+]
 
 ///////// add new enquiry //////
 router.post("/", enquiryAddValidator, enquiryController.addNewEnquiry);
